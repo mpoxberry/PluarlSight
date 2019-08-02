53d6094067d03a87091b8047d788fe47ea984540
Matthew Oxberry <Matthew.Oxberry@qvc.com>
2019-08-02 07:42:00 -0400

    adding parameter project

---
+++ Java/07_Parameters/Demo/CalcEngine/src/com/pluralsight/MathEquation.java
@@ -0,0 +1,50 @@
+package com.pluralsight;
+
+public class MathEquation {
+    private double leftVal;
+    private double rightVal;
+    private char opCode = 'a';
+    private double result;
+
+    public double getLeftVal() {
+        return leftVal;
+    }
+
+    public void setLeftVal(double leftVal) {
+        this.leftVal = leftVal;
+    }
+
+    public double getRightVal() {
+        return rightVal;
+    }
+
+    public void setRightVal(double rightVal) {
+        this.rightVal = rightVal;
+    }
+
+    public char getOpCode() {
+        return opCode;
+    }
+
+    public void setOpCode(char opCode) {
+        this.opCode = opCode;
+    }
+
+    public double getResult() {
+        return result;
+    }
+
+    public MathEquation() {}
+
+    public MathEquation(char opCode) {
+        this.opCode = opCode;
+    }
+
+    public MathEquation(char opCode, double leftVal, double rightVal) {
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
+
+
+}
