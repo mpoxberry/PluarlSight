90c1eeb269dea8a24d87d28ab0a580cdccc41b47
Matthew Oxberry <Matthew.Oxberry@qvc.com>
2019-08-06 06:57:05 -0400

    copied classes from previous project

--- Java/07_Parameters/Demo/CalcEngine/src/com/pluralsight/MathEquation.java
+++ Java/07_Parameters/Demo/CalcEngine/src/com/pluralsight/MathEquation.java
@@ -1,10 +1,60 @@
-package com.pluralsight;
+package com.pluralsight.calcengine;
 
+/**
+ * @author mpoxberry
+ * created 19-07-2019s
+ */
 public class MathEquation {
     private double leftVal;
     private double rightVal;
     private char opCode = 'a';
     private double result;
+
+    public MathEquation() {
+
+    };
+
+    public MathEquation(char opCode) {
+        this.opCode = opCode;
+    }
+
+    public MathEquation(char opCode, double leftVal, double rightVal) {
+        this(opCode);
+        this.leftVal = leftVal;
+        this.rightVal = rightVal;
+    }
+
+    public void execute() {
+        switch(opCode) {
+            case 'a': {
+                result = leftVal + rightVal;
+                break;
+            }
+            case 's': {
+                result = leftVal - rightVal;
+                break;
+            }
+            case 'd': {
+                result = rightVal != 0.0d ? leftVal / rightVal : 0.0d;
+//            if (val2 != 0.0d) {
+//                result = val1 / val2;
+//            }
+//            else {
+//                result = 0.0d;
+//            }
+                break;
+            }
+            case 'm': {
+                result = leftVal * rightVal;
+                break;
+            }
+            default: {
+                System.out.println("Error - invalid opCode");
+                result = 0.0d;
+                break;
+            }
+        }
+    }
 
     public double getLeftVal() {
         return leftVal;
@@ -33,18 +83,4 @@ public class MathEquation {
     public double getResult() {
         return result;
     }
-
-    public MathEquation() {}
-
-    public MathEquation(char opCode) {
-        this.opCode = opCode;
-    }
-
-    public MathEquation(char opCode, double leftVal, double rightVal) {
-        this(opCode);
-        this.leftVal = leftVal;
-        this.rightVal = rightVal;
-    }
-
-
 }
