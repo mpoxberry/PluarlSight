25fdc03ffabcd7ee97be99f88ddeea66f1708eb0
mpoxberry <mpoxberry@gmail.com>
2019-08-12 07:34:28 -0400

    added Adder class, extending CalculateBase class

---
+++ Java/08_Inheritance/Demo/CalcEngine/src/com/pluralsight/calcengine/Adder.java
@@ -0,0 +1,11 @@
+package com.pluralsight.calcengine;
+
+public class Adder extends CaclulateBase {
+    public Adder() {}
+
+    public Adder(double leftVal, double rightVal) {
+        super(leftVal, rightVal);
+    }
+
+    
+}
