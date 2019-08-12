9b88f9536fd8debdf283e287282e987aaf3ead88
mpoxberry <mpoxberry@gmail.com>
2019-08-12 07:53:12 -0400

    wired up all newly added classes

--- Java/08_Inheritance/Demo/CalcEngine/src/com/pluralsight/calcengine/Adder.java
+++ Java/08_Inheritance/Demo/CalcEngine/src/com/pluralsight/calcengine/Adder.java
@@ -9,7 +9,7 @@ public class Adder extends CalculateBase {
 
     @Override
     public void calculate() {
-        double value = getLeftVal() + getResult();
+        double value = getLeftVal() + getRightVal();
         setResult(value);
     }
 }
