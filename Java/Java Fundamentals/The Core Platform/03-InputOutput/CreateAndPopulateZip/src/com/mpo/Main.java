e3722ac49c746eaa735d55a71834a73d83dcfe47
mpoxberry <mpoxberry@gmail.com>
2019-09-25 08:56:17 -0400

    setup zip demo

---
+++ Java/Java Fundamentals/The Core Platform/03-InputOutput/CreateAndPopulateZip/src/com/mpo/Main.java
@@ -0,0 +1,36 @@
+package com.mpo;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.FileSystem;
+import java.nio.file.Path;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String[] data = {
+	            "Line 1",
+                "Line 2 2",
+                "Line 3 3 3",
+                "Line 4 4 4 4",
+                "Line 5 5 5 5 5"
+        };
+
+    }
+
+    private static FileSystem openZip(Path zipPath) throws IOException, URISyntaxException {
+
+    }
+
+    private static void copyToZip(FileSystem zipFs) throws IOException {
+
+    }
+
+    private static void writeToFileInZip1(FileSystem zipFs, String[] data) throws IOException {
+
+    }
+
+    private static void writeToFileInZip2(FileSystem zipFs, String[] data) throws IOException {
+        
+    }
+}
